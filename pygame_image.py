0f0ff90b2b106d42c1251b5391337aed9c745151
miyashita <c0a234179e@edu.teu.ac.jp>
2024-09-24 15:53:18 +0900

    練習7

--- pygame_image.py
+++ pygame_image.py
@@ -10,14 +10,17 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img,1,0)
     kuk_img = pg.image.load("fig/3.png")
     kuk_img = pg.transform.flip(kuk_img,1,0)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = -(tmr % 800)
+        x = -(tmr % 3200)
         screen.blit(bg_img, [x, 0])
+        screen.blit(bg_img2,[x+1600,0])
+        screen.blit(bg_img, [x+3200, 0])
         screen.blit(kuk_img,[300,200])
         pg.display.update()
         tmr += 1        
